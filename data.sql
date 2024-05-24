01ab6ba79e2b7ec95072676a1d1355c249e472a2
José Delpino <delpinoivivas@gmail.com>
2024-05-24 15:06:35 -0500

    Update books table schema with new columns and constraints

--- data.sql
+++ data.sql
@@ -2,12 +2,13 @@
 -- \c books-test;
 
 CREATE TABLE books (
-  isbn TEXT PRIMARY KEY,
-  amazon_url TEXT,
-  author TEXT,
-  language TEXT, 
+  id SERIAL PRIMARY KEY,
+  isbn VARCHAR(13) UNIQUE NOT NULL,
+  amazon_url VARCHAR(255),
+  author VARCHAR(255),
+  language VARCHAR(50),
   pages INTEGER,
-  publisher TEXT,
-  title TEXT, 
+  publisher VARCHAR(255),
+  title VARCHAR(255),
   year INTEGER
 );
